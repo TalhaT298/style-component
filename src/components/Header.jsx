97dc2e3329218d0a4c09ad1cfa68e87be3d666ba
Talha <mdtalha2008@gmail.com>
2023-04-16 03:09:47 +0600

    style add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import Nav from './Nav';
+
+// const Header = () => {
+//     return (
+//         <div>
+//             <NavLink to="/">
+//                 <img src='https://i.ibb.co/tw0nZC6/Untitled-design-10.png' alt='logo'></img>
+//             </NavLink>
+//             <Nav></Nav>
+//         </div>
+//     );
+// };
+
+// export default Header;
+import React from "react";
+import { NavLink } from "react-router-dom";
 import Nav from './Nav';
+import styled from "styled-components";
 
 const Header = () => {
-    return (
-        <div>
-            <NavLink to="/">
-                <img src='https://i.ibb.co/tw0nZC6/Untitled-design-10.png' alt='logo'></img>
-            </NavLink>
-            <Nav></Nav>
-        </div>
-    );
+  return (
+    <MainHeader>
+      <NavLink to="/">
+        <img src=".https://i.ibb.co/tw0nZC6/Untitled-design-10.png" alt="logo" className="logo" />
+      </NavLink>
+      <Nav></Nav>
+    </MainHeader>
+  );
 };
+
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    height: auto;
+    max-width: 30%;
+  }
+`;
 
 export default Header;
