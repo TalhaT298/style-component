f94232dd32a83104d4c88b690eb4c5e3c9276bd4
Talha <mdtalha2008@gmail.com>
2023-04-16 00:29:44 +0600

    start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Services from './Services';
+
+const App = () => {
+  return <>
+    <Home></Home>
+    <About></About>
+    <Services></Services>
+    <Contact></Contact>
+
+  </>
+};
+
+export default App;
